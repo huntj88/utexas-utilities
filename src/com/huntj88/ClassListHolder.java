9dcefbaf0f262c65eb087f338775d5098fda97d4
huntj88 <huntj88@gmail.com>
2015-09-08 16:57:08 -0500

    moving from laptop to desktop

---
+++ src/com/huntj88/ClassListHolder.java
@@ -0,0 +1,28 @@
+package com.huntj88;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.View;
+
+import java.util.ArrayList;
+
+/**
+ * Created by James on 9/8/2015.
+ */
+public class ClassListHolder{
+
+    ArrayList<UTClass> classList = new ArrayList<>();
+    int[][] classes = new int[5][20];
+
+    public ClassListHolder() {
+
+    }
+
+    public boolean addClass(UTClass utClass)
+    {
+
+        return false;
+    }
+
+
+}
